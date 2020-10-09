c2e3e61568a0da7ea8f0975e8fb23bc09197a7b8
Cris Matson <crisjmatson@gmail.com>
2020-10-09 17:22:35 -0400

    environment file typo fix

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -5,6 +5,6 @@ switch (window.location.hostname) {
 		APIURL = "http://localhost:3000";
 		break;
 	case "wesch.herokuapp.com/":
-		APIURL = "https://wesch-server.herokuapp.com/";
+		APIURL = "https://wesch-server.herokuapp.com";
 }
 export default APIURL;
