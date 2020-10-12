2587759a9fe48b56b79f3606009da7275a9f6a97
Cris Matson <crisjmatson@gmail.com>
2020-10-12 15:22:56 -0400

    env url rm

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
 	case "localhost" || "127.0.0.1":
 		APIURL = "http://localhost:3000";
 		break;
-	case "wesch.herokuapp.com/":
+	case "wesch.herokuapp.com":
 		APIURL = "https://wesch-server.herokuapp.com";
 }
 export default APIURL;
